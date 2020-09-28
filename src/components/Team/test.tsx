b5e8943ae66247c7192d181baa2b79147a3d8482
Christy Hauschild <christyhauschid@gmail.com>
2020-09-27 23:54:32 -0300

    test: Team component

---
+++ src/components/Team/test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Team from '.';
+
+const props = {
+  name: 'Barcelona',
+  description: 'Barcelona Squad',
+};
+
+describe('<Player />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<Team {...props} />);
+
+    expect(screen.getByText(props.name)).toBeInTheDocument();
+
+    expect(screen.getByText(props.description)).toBeInTheDocument();
+
+    expect(screen).toMatchSnapshot();
+  });
+});
