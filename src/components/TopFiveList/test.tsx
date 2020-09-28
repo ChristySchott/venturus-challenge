a28b7625304dcc98a4bf8b3c4ce034609cf274c7
Christy Hauschild <christyhauschid@gmail.com>
2020-09-28 00:42:53 -0300

    test: TopFiveList component

--- src/components/TopFiveList/test.tsx
+++ src/components/TopFiveList/test.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import TopFiveList from '.';
 
+const teams = [
+  {
+    id: '1',
+    name: 'AC Milan',
+    average: '31.9',
+  },
+  {
+    id: '2',
+    name: 'FC Nantes',
+    average: '28.3',
+  },
+  {
+    id: '3',
+    name: 'Grêmio',
+    average: '27.9',
+  },
+  {
+    id: '4',
+    name: 'Inter',
+    average: '25.6',
+  },
+];
+
+const props = {
+  title: 'Highest avg age',
+  list: teams,
+};
+
 describe('<TopFiveList />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<TopFiveList />);
+  it('should render the first list name', () => {
+    renderWithTheme(<TopFiveList {...props} />);
 
-    expect(
-      screen.getByRole('heading', { name: /TopFiveList/i }),
-    ).toBeInTheDocument();
+    expect(screen.getByText(props.list[0].name)).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
+  });
+
+  it('should render all items in the list', () => {
+    renderWithTheme(<TopFiveList {...props} />);
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(4);
   });
 });
