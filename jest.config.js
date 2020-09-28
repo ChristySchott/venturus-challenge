7592d14eb7cf803f86bb69f0ce6860eb4a7c9e7f
Christy Hauschild <christyhauschid@gmail.com>
2020-09-27 21:57:15 -0300

    feat: config babel and jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  preset: "ts-jest",
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
 }
