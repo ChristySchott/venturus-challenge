68a80e7ebee479f28364fdf6b9f3b45ddef83c81
Christy Hauschild <christyhauschid@gmail.com>
2020-09-28 20:47:21 -0300

    test: radio component

---
+++ src/components/Radio/test.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+import theme from 'styles/theme';
+
+import Radio from '.';
+
+const radios = [
+  {
+    id: '1',
+    value: '',
+    label: 'Real',
+  },
+  {
+    id: '2',
+    value: '',
+    label: 'Fantasy',
+  },
+];
+
+const props = {
+  title: 'Team types',
+  radioList: radios,
+};
+
+describe('<Radio />', () => {
+  it('should render the input radios correctly', () => {
+    renderWithTheme(<Radio {...props} />);
+
+    expect(screen.getAllByRole('radio')).toHaveLength(2);
+
+    expect(screen).toMatchSnapshot();
+  });
+
+  it('should render the title', () => {
+    renderWithTheme(<Radio {...props} />);
+
+    expect(
+      screen.getByRole('heading', { name: props.title }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render the title with a red font', () => {
+    renderWithTheme(<Radio {...props} error />);
+
+    expect(screen.getByText(props.title)).toHaveStyle({
+      color: theme.colors.red,
+    });
+  });
+});
