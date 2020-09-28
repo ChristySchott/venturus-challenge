7932900c38a6d717e60f00ccaf08a4cd93205fa4
Christy Hauschild <christyhauschid@gmail.com>
2020-09-27 22:08:10 -0300

    feat: create MostLessPickedPlayer component

---
+++ src/components/MostLessPickedPlayers/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import {
+  Wrapper,
+  MostPicked,
+  MidField,
+  MidFieldLine,
+  LessPicked,
+  PlayerInfos,
+  RatingsContainer,
+} from './styles';
+
+export type PickedPlayerProps = {
+  name: string;
+  initials: string;
+  rating: string;
+};
+
+export type MostLessPickedPlayersProps = {
+  mostPicked: PickedPlayerProps;
+  lessPicked: PickedPlayerProps;
+};
+
+const MostLessPickedPlayers: React.FC<MostLessPickedPlayersProps> = ({
+  mostPicked,
+  lessPicked,
+}) => (
+  <Wrapper>
+    <MostPicked>
+      <PlayerInfos>
+        <h1>{mostPicked.name}</h1>
+        <div>
+          <h2>{mostPicked.initials}</h2>
+          <RatingsContainer>
+            <h3>{mostPicked.rating}</h3>
+          </RatingsContainer>
+        </div>
+      </PlayerInfos>
+    </MostPicked>
+
+    <MidField />
+    <MidFieldLine />
+
+    <LessPicked>
+      <PlayerInfos>
+        <h1>{lessPicked.name}</h1>
+        <div>
+          <h2>{lessPicked.initials}</h2>
+          <RatingsContainer>
+            <h3>{lessPicked.rating}</h3>
+          </RatingsContainer>
+        </div>
+      </PlayerInfos>
+    </LessPicked>
+  </Wrapper>
+);
+
+export default MostLessPickedPlayers;
---
+++ src/components/MostLessPickedPlayers/styles.ts
@@ -0,0 +1,154 @@
+import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    background-image: ${theme.colors.lightenGradient};
+    padding: ${theme.spacings.small} ${theme.spacings.xsmall};
+    text-align: center;
+
+    ${media.greaterThan('medium')`
+      flex-direction: row;
+      justify-content: space-between;
+      padding: ${theme.spacings.medium} ${theme.spacings.large};
+    `}
+  `}
+`;
+
+export const MostPicked = styled.div`
+  ${({ theme }) => css`
+    h2 {
+      position: relative;
+
+      &::after {
+        content: '';
+        position: absolute;
+        top: -8px;
+        left: -8px;
+        right: -8px;
+        bottom: -8px;
+        border: ${theme.colors.primary} 2px dashed;
+        box-shadow: 1px 1px 1px 1px outset green;
+
+        -moz-border-radius: 50%;
+        -webkit-border-radius: 50%;
+        border-radius: 50%;
+      }
+    }
+  `}
+`;
+
+export const MidField = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+
+    width: 130px;
+    height: 130px;
+    -moz-border-radius: 50%;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+
+    border: 1px solid ${theme.colors.lightGray};
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  `}
+`;
+
+export const MidFieldLine = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+
+    width: 100%;
+
+    border: 0.5px solid ${theme.colors.lightGray};
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+
+    ${media.greaterThan('medium')`
+      left: 50%;
+      width: 0;
+      height: 100%;
+    `}
+  `}
+`;
+
+export const LessPicked = styled.div`
+  ${({ theme }) => css`
+    margin-top: 220px;
+
+    ${media.greaterThan('medium')`
+      margin-top: 0;
+    `}
+
+    h2 {
+      border: 2px solid ${theme.colors.red};
+    }
+  `}
+`;
+
+export const PlayerInfos = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      color: ${theme.colors.white};
+      margin-bottom: ${theme.spacings.xsmall};
+      font-size: ${theme.font.sizes.xxlarge};
+    }
+
+    > div {
+      display: flex;
+      flex-direction: column;
+
+      ${media.greaterThan('medium')`
+        flex-direction: row;
+      `}
+    }
+
+    h2 {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+      background: ${theme.colors.white};
+      font-size: ${theme.font.sizes.xxlarge};
+    }
+  `}
+`;
+
+export const RatingsContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    color: ${theme.colors.white};
+
+    h3 {
+      color: ${theme.colors.white};
+      font-size: ${theme.font.sizes.xxlarge};
+      border-bottom: 4px solid ${theme.colors.white};
+      margin-top: ${theme.spacings.xsmall};
+
+      ${media.greaterThan('medium')`
+        margin-top: 0;
+        margin-left: ${theme.spacings.small};
+        height: 25px;
+
+        font-size: ${theme.font.sizes.large};
+        border-bottom: 3px solid ${theme.colors.white};
+      `}
+    }
+  `}
+`;
