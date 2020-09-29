36cdb4dc43b9efc3c19b87ba87f62435d5b0548a
Christy Hauschild <christyhauschid@gmail.com>
2020-09-28 21:51:38 -0300

    feat: add header to pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MyTeams from 'pages/MyTeams';
 import CreateTeam from 'pages/CreateTeam';
 
+import Header from 'components/Header';
+
 const Routes = () => (
   <BrowserRouter>
     <Switch>
+      <Header user="Christy Hauschild" />
       <Route path="/my-teams" exact component={MyTeams} />
       <Route path="/create-team" component={CreateTeam} />
     </Switch>
