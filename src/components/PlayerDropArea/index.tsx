dde5a37006482d5c37e0204bf653aabe76ba9a57
Christy Hauschild <christyhauschid@gmail.com>
2020-09-29 19:14:38 -0300

    feat: add dropzone logic to PlayerDropArea

--- src/components/PlayerDropArea/index.tsx
+++ src/components/PlayerDropArea/index.tsx
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Plus } from '@styled-icons/boxicons-regular/Plus';
+import { useDrop, DragObjectWithType } from 'react-dnd';
 
 import { Player } from 'shared/types/Player';
+import { PlayerPositions } from 'shared/types/Team';
+
 import Tooltip from 'components/Tooltip';
 
 import { Wrapper } from './styles';
 
+type DropType = DragObjectWithType & { player: PlayerPositions };
+
 export type PlayerDropAreaProps = {
-  player?: Player;
+  row: number;
+  col: number;
+  formation: number[];
+  selectPlayer: (player: Player) => void;
+  currentPlayer: PlayerPositions | undefined;
 };
 
-const PlayerDropArea = ({ player }: PlayerDropAreaProps) => (
-  <Wrapper>
-    <div>
-      {player && <Tooltip player={player} />}
-      {!player && <Plus aria-label="Add a player" data-testid="plus-icon" />}
-    </div>
-  </Wrapper>
-);
+const PlayerDropArea = (props: PlayerDropAreaProps) => {
+  const { selectPlayer, row, col, formation, currentPlayer } = props;
+
+  const position = [row, col];
+
+  const [sheddedPlayer, setSheddedPlayer] = useState<
+    PlayerPositions | undefined
+  >(currentPlayer);
+
+  useEffect(() => {
+    setSheddedPlayer(undefined);
+  }, [formation]);
+
+  const [, drop] = useDrop({
+    accept: 'player',
+    drop: ({ player }: DropType) => {
+      const teamPlayer = { ...player, position };
+
+      setSheddedPlayer(teamPlayer);
+      selectPlayer(teamPlayer);
+    },
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
+    }),
+  });
+
+  const player = sheddedPlayer || currentPlayer;
+
+  return (
+    <Wrapper ref={drop}>
+      <div>
+        {player && <Tooltip player={player} />}
+        {!player && <Plus aria-label="Add a player" data-testid="plus-icon" />}
+      </div>
+    </Wrapper>
+  );
+};
 
 export default PlayerDropArea;
