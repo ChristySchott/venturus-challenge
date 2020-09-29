a3197cf995226011dd58f563ead590647dff9aab
Christy Hauschild <christyhauschid@gmail.com>
2020-09-29 19:32:43 -0300

    feat: add DndProvider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import Routes from 'routes';
 import GlobalStyles from './styles/global';
@@ -8,10 +10,22 @@ import theme from './styles/theme';
 
 export type ThemeType = typeof theme;
 
+export function collect(
+  connect: { dragSource: () => any },
+  monitor: { isDragging: () => any },
+) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  };
+}
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      <DndProvider backend={HTML5Backend}>
+        <Routes />
+      </DndProvider>
       <GlobalStyles />
     </ThemeProvider>
   );
