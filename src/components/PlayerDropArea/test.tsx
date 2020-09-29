b85abc44e3927a63d1a6877507b983fc5f079240
Christy Hauschild <christyhauschid@gmail.com>
2020-09-29 11:01:55 -0300

    test: PlayerDropArea component

---
+++ src/components/PlayerDropArea/test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import PlayerDropArea from '.';
+
+const props = {
+  player: {
+    id: '1',
+    name: 'Christy',
+    age: '19',
+    nationality: 'Brazil',
+  },
+};
+
+describe('<PlayerDropArea />', () => {
+  it('should render the player name', () => {
+    renderWithTheme(<PlayerDropArea {...props} />);
+
+    expect(screen.getByText(props.player.name)).toBeInTheDocument();
+
+    expect(screen).toMatchSnapshot();
+  });
+
+  it('should render the plus icon', () => {
+    renderWithTheme(<PlayerDropArea />);
+
+    expect(screen.getByTestId('plus-icon')).toBeInTheDocument();
+
+    expect(screen).toMatchSnapshot();
+  });
+});
