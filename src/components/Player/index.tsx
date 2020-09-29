a9b8178a27591b7780c158fa3a890228aea1516a
Christy Hauschild <christyhauschid@gmail.com>
2020-09-29 19:35:00 -0300

    fix: change accept type name

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,34 +1,49 @@
 import React from 'react';
+import { DragSource, useDrag } from 'react-dnd';
+
+import { collect } from 'App';
+import { Player } from 'shared/types/Player';
 
 import { Wrapper, Info } from './styles';
 
 export type PlayerProps = {
-  name: string;
-  age: string;
-  nationality: string;
+  player: Player;
 };
 
-const Player = ({ name, age, nationality }: PlayerProps) => (
-  <Wrapper>
-    <div>
-      <div>
-        <Info>
-          <span>Name:</span>
-          {name}
-        </Info>
-        <Info>
-          <span>Age:</span>
-          {age}
-        </Info>
-      </div>
-      <div>
-        <Info>
-          <span>Nationality:</span>
-          {nationality}
-        </Info>
-      </div>
-    </div>
-  </Wrapper>
-);
+export const playerSrc = {
+  beginDrag(props: { id: string }) {
+    const item = { id: props.id };
+    return item;
+  },
+};
 
-export default Player;
+const Player = ({ player }: PlayerProps) => {
+  const [, dragRef] = useDrag({
+    item: { type: 'player', player },
+  });
+
+  return (
+    <Wrapper ref={dragRef}>
+      <div>
+        <div>
+          <Info>
+            <span>Name:</span>
+            {player.name}
+          </Info>
+          <Info>
+            <span>Age:</span>
+            {player.age}
+          </Info>
+        </div>
+        <div>
+          <Info>
+            <span>Nationality:</span>
+            {player.nationality}
+          </Info>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default DragSource('player', playerSrc, collect)(Player);
